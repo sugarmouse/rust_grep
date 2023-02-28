d014641fedab3f1022a5e349ad21bee675c4108d
sugarmouse <tanghao19950911@gmail.com>
2023-03-01 00:45:53 +0800

    模块化和错误处理

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
 // cargo run -- searchstring example-filename.txt
 use std::env;
-use std::fs;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path)
-        .expect("should have been able to read the file");
-
-    println!("with text:\n{contents}")
-
-
+    if let Err(e) =  minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
